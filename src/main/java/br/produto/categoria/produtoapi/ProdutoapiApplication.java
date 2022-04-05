638d67dcac1217737d3603f54361f472c578e9a1
Matheus Abreu <matheusa596@gmail.com>
2022-04-05 20:16:26 -0300

    Enviando classes Main do projeto.

---
+++ src/main/java/br/produto/categoria/produtoapi/ProdutoapiApplication.java
@@ -0,0 +1,13 @@
+package br.produto.categoria.produtoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/produto/categoria/produtoapi/ProdutoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.produto.categoria.produtoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
